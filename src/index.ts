fd9d119024a78d5453e2f75126780a5ce49fb94b
WildanFathir <wildanfathir612@gmail.com>
2025-11-26 15:32:47 +0700

    feat: refactor server initialization to use dokterRoute

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Elysia } from 'elysia';
+import {dokterRoute} from './routes/dokter.route'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(process.env.PORT || 3001);
+const app = new Elysia()
+  .use(dokterRoute)
+  .listen(process.env.PORT || 3001);
 
 console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
