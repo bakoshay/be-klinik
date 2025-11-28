06b8e59b4794ac5c1c10e132e6112c766c1e9b35
WildanFathir <wildanfathir612@gmail.com>
2025-11-28 20:14:12 +0700

    feat: integrate Antrian routes and WebSocket into the main application

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Elysia } from 'elysia';
 import {privateDokterRoute, publicDokterRoute} from './routes/dokter.route'
+import { publicAntrianRoute, privateAntrianRoute, antrianWebSocket } from './routes/antrian.route'
 import { authRoute } from './routes/auth.route'
 import { cors } from '@elysiajs/cors'
 import {jwtPlugin} from './middleware/auth'
@@ -7,7 +8,8 @@ import {jwtPlugin} from './middleware/auth'
 const port = process.env.PORT || 3001;
 const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
 
-const app = new Elysia()
+const app = new Elysia();
+app
   .use(
     cors({
       origin: frontendUrl,
@@ -16,8 +18,12 @@ const app = new Elysia()
   )
   .use(jwtPlugin)
   .use(authRoute)
+  .use(publicAntrianRoute)
+  .use(privateAntrianRoute)
+  .use(antrianWebSocket(app))
   .use(publicDokterRoute)
   .use(privateDokterRoute)
   .listen({ port });
 
 console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
+console.log(`🔌 WebSocket ready at ws://localhost:${port}/ws/antrian`);
