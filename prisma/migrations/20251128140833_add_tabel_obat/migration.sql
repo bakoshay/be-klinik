f76d096e7efb9b81f81f6eb3d96e513f8f414705
WildanFathir <wildanfathir612@gmail.com>
2025-11-28 21:27:59 +0700

    feat: add Obat model and migration for medication management

---
+++ prisma/migrations/20251128140833_add_tabel_obat/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Obat` (
+    `id` VARCHAR(191) NOT NULL,
+    `nama` VARCHAR(191) NOT NULL,
+    `jenis` ENUM('tablet', 'kapsul', 'kaplet') NOT NULL,
+    `harga` INTEGER NOT NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
